5ac3ff2f74cee4e774918b2eb93b72eeff0f5ca0
nene-hue <neomontshiwa2@gmail.com>
2025-11-07 15:15:08 +0200

    imported functions from the store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,9 +13,7 @@ import { authReducer } from './store/reducers/auth.reducers';
 import { taskReducer } from './store/reducers/task.reducers';
 import { TaskEffects } from './store/effects/task.effects';
 import { TaskListComponent } from './components/task-list/task-list.component';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 
 @NgModule({
   declarations: [
@@ -26,7 +24,10 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ComponentsModule
+    ComponentsModule,
+    StoreModule.forRoot({tasks:
+      taskReducer
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-
+import { ToDo } from 'src/app/models/models';
+import { loadCurrentUserTasks, loadCurrentUserTasksFailure, loadCurrentUserTasksSuccess, clearTaskError, createTaskFailure, createTaskSuccess, createTaskForCurrentUser,deleteTask, deleteCurrentUserTask, deleteTaskFailure, deleteTaskSuccess, updateCurrentUserTaskStatus, updateTask, updateTaskStatusFailure, updateTaskStatusSuccess } from 'src/app/store/actions/task.actions';
 
 @Component({
   selector: 'app-task-list',
