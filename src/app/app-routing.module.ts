91c85f770c919395b4f47613500d27cd869f5b26
DA MAC M1 133 <damacm1133@DAs-MacBook-Air.local>
2025-11-10 12:30:20 +0200

    dashboard funtionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './components/authentication/authentication.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TaskManagerComponent } from './components/task-manager/task-manager.component';
 import { TaskFormComponent } from './components/task-form/task-form.component';
 
 
@@ -10,8 +11,8 @@ const routes: Routes = [
   // { path: 'dashboard', component: DashboardComponent, canActivate: [AuthenticationComponent] },
   { path: 'dashboard', component: DashboardComponent },
   {path: 'auth', component: AuthenticationComponent},
-  // { path: '**', redirectTo: '/auth' },
-  { path: 'task-form', component: TaskFormComponent }
+  { path: 'tasks', component: TaskFormComponent },
+  { path: '**', redirectTo: '/auth' }
 ];
 
 @NgModule({
